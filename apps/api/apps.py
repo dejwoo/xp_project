5772fd9565c4439beaa3ac7aead48107c03d7459
dejwoo <david.koszeghy@gmail.com>
2017-04-26 09:17:37 +0200

    fix: duplicate messages

--- apps/api/apps.py
+++ apps/api/apps.py
@@ -14,15 +14,27 @@ class ApiConfig(AppConfig):
         self.Message = self.get_model('Message')
         self.TxInfo = self.get_model('TxInfo')
         self.RxInfo = self.get_model('RxInfo')
-        Signal.connect(self.mqtt.signal,receiver=self.on_message_recieved, weak=True)
-    def on_message_recieved(self, sender, message,txInfo,rxInfo,**kwargs):
+        Signal.connect(self.mqtt.getSignal(),receiver=self.on_message_recieved, weak=False, dispatch_uid="dd5e8bfe-ea9b-42a3-a595-4810d0987650")
+        return
+
+    def on_message_recieved(self, sender, message, txInfo, rxInfo, **kwargs):
         dataRate = txInfo.pop('dataRate', None)
         newTxInfo = self.TxInfo(*txInfo)
         newRxInfo = self.RxInfo(*rxInfo)
-        newMessage = self.Message()
-        for key,value in message.items():
-            newMessage.key = value
-        print("Message", newMessage)
-        print("Message", message)
+        # newMessage = self.Message(
+        #     applicationName = message.applicationName,
+        #     applicationID = message.applicationID,
+        #     devEUI = message.devEUI,
+        #     nodeName = message.nodeName,
+        #     data = message.data,
+        #     fCnt = message.fCnt,
+        #     fPort = message.fPort,
+        #     gateway = message.gateway,
+        #     node = message.node,
+        #     timestamp = message.timestamp,
+        #     rxInfo = message.rxInfo,
+        #     txInfo = message.txInfo)
+        # print("Message", newMessage)
+        print("Message data", message['data'])
         # print("txInfo", newTxInfo)
         # print("rxInfo", newRxInfo)
