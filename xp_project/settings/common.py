133be5202cff168f21fa99de5550ad510900b89e
Ákos <akoshervay@gmail.com>
2017-04-27 12:46:57 +0200

    develop: changed permission classes

--- xp_project/settings/common.py
+++ xp_project/settings/common.py
@@ -109,8 +109,9 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.AllowAny',
     ),
+    'PAGE_SIZE': 10,
     'TEST_REQUEST_DEFAULT_FORMAT': 'json',
 }
 
